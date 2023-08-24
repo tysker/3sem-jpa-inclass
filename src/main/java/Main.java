c8ee1af30d76c2efd088691cfa321989e9a9b4f6
Jörg Oertel <joergoertel@proton.me>
2023-08-24 16:28:13 +0200

    commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,35 @@
-public class Main {
-    public static void main(String[] args) {
+import jakarta.persistence.EntityManagerFactory;
+import model.EntityA;
+import model.EntityB;
+import model.MyEnum;
 
+public class Main {
+
+    public static void main(String[] args) {
+        EntityManagerFactory emf = HibernateConfig.getEntityManagerFactoryConfig();
+//        myFirstPersist(emf);
+
+        try(var em = emf.createEntityManager()) {
+            EntityA entityA = em.find(EntityA.class, 1);
+
+            em.detach(entityA);
+            entityA.setPrice(100.0D);
+
+        }
     }
+
+    private static void myFirstPersist(EntityManagerFactory emf) {
+        EntityA entityA = new EntityA("A", 55.58D, MyEnum.ConstA);
+        EntityB entityB = new EntityB("B");
+        entityA.setEntityB(entityB);
+        try (var em = emf.createEntityManager()) {
+            em.getTransaction().begin();
+            em.persist(entityA);
+            em.persist(entityB);
+            em.persist(new EntityA("C", 154.69D, MyEnum.ConstC));
+            em.getTransaction().commit();
+        }
+    }
+
+
 }
---
+++ src/main/java/model/EntityA.java
@@ -0,0 +1,45 @@
+package model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@NoArgsConstructor
+@Getter
+@ToString
+@Entity
+@Table(name = "entitya")
+public class EntityA {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private int id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "price")
+    private double price;
+
+    @Enumerated(EnumType.STRING)
+    private MyEnum myEnum;
+
+    @OneToOne
+    private EntityB entityB;
+
+    public EntityA(String name, double price, MyEnum myEnum) {
+        this.name = name;
+        this.price = price;
+        this.myEnum = myEnum;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public void setEntityB(EntityB entityB) {
+        this.entityB = entityB;
+    }
+}
