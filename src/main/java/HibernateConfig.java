eb0d3db61b4814564cd73b88b6771be92eb36fd5
Jörg Oertel <joergoertel@proton.me>
2023-08-25 07:53:44 +0200

    refactored

--- src/main/java/HibernateConfig.java
+++ src/main/java/HibernateConfig.java
@@ -21,9 +21,9 @@ public class HibernateConfig {
             props.put("hibernate.connection.url", "jdbc:postgresql://localhost:5432/exercise?currentSchema=public");
             props.put("hibernate.connection.username", "postgres");
             props.put("hibernate.connection.password", "postgres");
-            props.put("hibernate.show_sql", "true"); // show sql in console
-            props.put("hibernate.format_sql", "true"); // format sql in console
-            props.put("hibernate.use_sql_comments", "true"); // show sql comments in console
+            props.put("hibernate.show_sql", "false"); // show sql in console
+            props.put("hibernate.format_sql", "false"); // format sql in console
+            props.put("hibernate.use_sql_comments", "false"); // show sql comments in console
 
             props.put("hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect"); // dialect for postgresql
             props.put("hibernate.connection.driver_class", "org.postgresql.Driver"); // driver class for postgresql
